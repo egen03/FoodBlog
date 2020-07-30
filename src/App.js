3574dd7884e6756ca54b7f492bc28c43b3941913
Student_GA111 <egen03@gmail.com>
2020-07-30 09:01:37 -0400

    added bootstrap nav documentation

--- src/App.js
+++ src/App.js
@@ -10,19 +10,9 @@ export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+      <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+  {/* <!-- Navbar content --> */}
+      </nav>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
