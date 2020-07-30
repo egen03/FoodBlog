aebac24a0eab2b7a304917df165cfc9fad28afa8
Student_GA111 <egen03@gmail.com>
2020-07-30 11:22:07 -0400

    imported info to Home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Title from './Title'
+import Title from './Title';
+import Info from './Info';
 
 const Home = () => {
     return(
        <div className="Title">
             <Title title="Eating with Erica" />
+            
         </div>
     )
 
