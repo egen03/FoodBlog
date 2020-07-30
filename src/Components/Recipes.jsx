7f22096bd017d33006a9946ca2aa6f4f97c18f68
Student_GA111 <egen03@gmail.com>
2020-07-30 19:29:50 -0400

    changed class syntax to className

--- src/Components/Recipes.jsx
+++ src/Components/Recipes.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Recipes = () => {
     return(
-        <div class="card" style="width: 18rem;">
+        <div className="card" style="width: 18rem;">
   
-            <img src="..." class="card-img-top" alt="..." />
+            <img src="..." className="card-img-top" alt="..." />
 
-             <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+             <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
 
             </div>
