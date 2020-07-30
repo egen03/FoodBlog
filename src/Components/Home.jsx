236414013bfb5d3b6f7dfa55dcbbe4c479349600
Student_GA111 <egen03@gmail.com>
2020-07-30 15:37:23 -0400

    something is wrong deleted info component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
     return(
        <div className="Title">
             <Title title="Eating with Erica" />
+            <Info info="Cooking as a family or with friends and loved ones brings us together because we are able to share our culture and heritage through food. When you’re feeling down, what’s better than talking about it to your friends or family while eating some delicious food? Sharing food with a friend during times of trouble not only nourishes our bodies but also our souls. With food we make friends, court lovers, and count our blessings. The sharing of food has always been part of the human story!" />>
             
         </div>
     )
