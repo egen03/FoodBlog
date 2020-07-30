2befbb208ad75de293eeb3eb0cce3a92f1e19b69
Student_GA111 <egen03@gmail.com>
2020-07-30 18:40:12 -0400

    imported info to home

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Home";
-import Title from './Components/Title';
+
 
 export default function App() {
   return (
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,13 +4,11 @@ import Info from './Info';
 
 const Home = () => {
     return(
-       <div className="Title">
+       <div className="title">
             <Title title="Eating with Erica" />
-            <Info info="Cooking as a family or with friends and loved ones brings us together because we are able to share our culture and heritage through food. When you’re feeling down, what’s better than talking about it to your friends or family while eating some delicious food? Sharing food with a friend during times of trouble not only nourishes our bodies but also our souls. With food we make friends, court lovers, and count our blessings. The sharing of food has always been part of the human story!" />>
-            
-        </div>
-    )
-
+        </div>  
+        
+    );
 }
 
 export default Home;
