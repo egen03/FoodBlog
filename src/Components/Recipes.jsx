9a29a128e94a16acc513a36d8dbb807be44639b2
Student_GA111 <egen03@gmail.com>
2020-07-30 22:10:58 -0400

    added image and text for thai food

--- src/Components/Recipes.jsx
+++ src/Components/Recipes.jsx
@@ -7,12 +7,14 @@ const Recipes = () => {
         
             <div class="card">
 
-                <img src="..." class="card-img-top" alt="..." />
+                <div className="foodImage">
+                <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/foodblog/foodblog/src/Images/thai.jpeg")} class="card-img-top" alt="Thai Food" height="500px" width="700px" />
+                </div>
 
             <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">Thai Food</h5>
+            <p class="card-text">Thai cooking places emphasis on lightly prepared dishes with strong aromatic components and a spicy edge. Thai cooking is about the juggling of disparate elements to create a harmonious finish.</p>
+            <a href="#" class="btn btn-primary">Thai Recipes</a>
             </div>
 
             </div>
