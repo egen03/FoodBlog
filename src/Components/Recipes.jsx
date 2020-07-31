dfd61e359ff1f4222b03bb90063908aa0ff621bd
Student_GA111 <egen03@gmail.com>
2020-07-30 23:59:01 -0400

    customized 3rd card

--- src/Components/Recipes.jsx
+++ src/Components/Recipes.jsx
@@ -29,7 +29,7 @@ const Recipes = () => {
             <div class="card col mb-4">
 
                 <div className="foodImage">
-                <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/foodblog/foodblog/src/Images/italian.jpeg")} class="card-img-top" alt="Thai Food" height="500px" width="700px" />
+                <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/foodblog/foodblog/src/Images/italian.jpeg")} class="card-img-top" alt="Italian Food" height="500px" width="700px" />
                 </div>
 
             <div class="card-body">
@@ -43,13 +43,13 @@ const Recipes = () => {
             <div class="card col mb-4">
 
                 <div className="foodImage">
-                <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/foodblog/foodblog/src/Images/italian.jpeg")} class="card-img-top" alt="Thai Food" height="500px" width="700px" />
+                <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/foodblog/foodblog/src/Images/french.jpeg")} class="card-img-top" alt="French Food" height="500px" width="700px" />
                 </div>
 
             <div class="card-body">
-            <h5 class="card-title">Italian Food</h5>
-            <p class="card-text">Italian cuisine is known for its regional diversity, especially between the north and the south of the Italian peninsula. It offers an abundance of taste, and is one of the most popular and copied in the world. Italian cuisine is generally characterized by its simplicity, with many dishes having only two to four main ingredients. Italian cooks rely chiefly on the quality of the ingredients rather than on elaborate preparation</p>
-            <a href="#" class="btn btn-primary">Italian Recipes</a>
+            <h5 class="card-title">French Food</h5>
+            <p class="card-text">There are many dishes that are considered part of French national cuisine today. A meal often consists of three courses, hors d'œuvre or entrée (introductory course, sometimes soup), plat principal (main course), fromage (cheese course) or dessert, sometimes with a salad offered before the cheese or dessert. French regional cuisine is characterized by its extreme diversity and style. Traditionally, each region of France has its own distinctive cuisine.</p>
+            <a href="#" class="btn btn-primary">French Recipes</a>
             </div>
 
             </div>
