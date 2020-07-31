63a5fc90b5c725b0e80f16509a009256773c65f3
Student_GA111 <egen03@gmail.com>
2020-07-30 23:47:00 -0400

    added layout for 3rd card

--- src/Components/Recipes.jsx
+++ src/Components/Recipes.jsx
@@ -40,6 +40,20 @@ const Recipes = () => {
 
             </div>
 
+            <div class="card col mb-4">
+
+                <div className="foodImage">
+                <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/foodblog/foodblog/src/Images/italian.jpeg")} class="card-img-top" alt="Thai Food" height="500px" width="700px" />
+                </div>
+
+            <div class="card-body">
+            <h5 class="card-title">Italian Food</h5>
+            <p class="card-text">Italian cuisine is known for its regional diversity, especially between the north and the south of the Italian peninsula. It offers an abundance of taste, and is one of the most popular and copied in the world. Italian cuisine is generally characterized by its simplicity, with many dishes having only two to four main ingredients. Italian cooks rely chiefly on the quality of the ingredients rather than on elaborate preparation</p>
+            <a href="#" class="btn btn-primary">Italian Recipes</a>
+            </div>
+
+            </div>
+
         </div>
         </div>
     )
