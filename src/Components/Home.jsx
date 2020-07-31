2d7cb69d9f6bf953285c2dfe66d0c3e1672a2403
Student_GA111 <egen03@gmail.com>
2020-07-30 21:22:23 -0400

    added text to homepage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ export class Home extends React.Component {
                 <Title title="Eating with Erica" />
             </div>
             <div>
-                <Info info="info text"/>
+                <Info info="Cooking as a family or with friends and loved ones brings us together because we are able to share our culture and heritage through food and give each other nourishment. Food helps us find our way closer to each other, as we gather together to prepare a meal and sit down for dinner. In these moments of uncertainty, it is the familiar feeling food brings that offers the most comfort. With food we make friends, court lovers, and count our blessings. The sharing of food has always been part of the human story. "/>
             </div>
             </div>
         )
